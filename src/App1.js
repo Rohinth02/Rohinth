ce4928869efd6d3cfb69a2237f75cd0bfe5f3853
Rohinth <124781757+Rohinth02@users.noreply.github.com>
2023-11-07 15:47:20 +0530

    Create App1.js

---
+++ src/App1.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Main from './Main';
+import Footer from './Footer';
+function App() {
+  return(
+
+<div>
+  <Navbar></Navbar>
+  <Main></Main>
+  <Footer></Footer>
+  </div>
+  );
+}
+
+export default App;
+
