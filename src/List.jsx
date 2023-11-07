00ac8bdcbb00cbe5239d4c0a5253c2c5dfa35ce4
ohinth <727722euit149@skcet.ac.in>
2023-11-07 11:37:16 +0530

    First git

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+function List()
+{
+    return (
+        <div>
+            <ul>
+                <li>Hi</li>
+                <li>Hello</li>
+                <li>List</li>
+                
+            </ul>
+        </div>
+    );
+}
+export {List};
---
+++ src/Message.jsx
@@ -0,0 +1,10 @@
+function Message()
+{
+    return (
+        <div>
+            <h1>From Message.jsx</h1>
+            <h2>Hello World</h2>
+        </div>
+    );
+}
+export {Message};
