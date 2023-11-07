134c2352c2d85cc1d2e65ac509fcc198eccfd7fc
ohinth <727722euit149@skcet.ac.in>
2023-11-07 15:52:10 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import { Message } from './Message';
-import { List } from './List';
 
+import './App.css';
+import Navbar from './Navbar';
+import Main from './Main';
+import Footer from './Footer';
 function App() {
   return(
 
 <div>
-  <Message></Message>
-   <List></List>
+  <Navbar></Navbar>
+  <Main></Main>
+  <Footer></Footer>
   </div>
   );
 }
 
 export default App;
+
