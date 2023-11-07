fdc7f264353d4badb2c6689970564f9d7f268d12
ohinth <727722euit149@skcet.ac.in>
2023-11-07 16:21:17 +0530

    kjhijh

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,34 +1,20 @@
-
 import React from "react";
 
 const Main= () =>{
     return(
         <div>
-            <center>
-
                <label for="name">Email Id: &emsp;</label>
               <input  id="name" type="box" placeholder="Email ID"></input>
-           <pre>
-
-
-           </pre>
+           <br>
+           </br>
                <label for="na">Password: &emsp;</label>
              <input id="na" type="box" placeholder="Password"></input>
-           <pre>
-
-
-           </pre>
+           <br>
+           </br>
                <label for="n">Submit: &emsp;</label>
             <input id="n" type="box" placeholder="click"></input>
-           <pre>
 
-
-           </pre>
-            &emsp;
-            &emsp;
-            &emsp;
-            <button >Submit</button>
-            </center>
+            <input></input>
 
 
         </div>
